38027556f5bc1f2d56bf21e2cd565ce1de07e1c6
Sebastian Nowak <sebastian.nowak@hexagram.com>
2017-03-09 17:57:16 +0100

    added first version of script

---
+++ shell-autostart.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+AUTOSTART_FILE='.autostart'
+
+function set_shell_autostart {
+    if [ -f ${AUTOSTART_FILE} ]; then
+        source ${AUTOSTART_FILE}
+    fi
+}
+
+function shell_autostart_install {
+  if [ -z "$PROMPT_COMMAND" ]; then
+    PROMPT_COMMAND=set_shell_autostart
+  else
+    PROMPT_COMMAND=${PROMPT_COMMAND%% }; # remove trailing spaces
+    PROMPT_COMMAND=${PROMPT_COMMAND%\;}; # remove trailing semi-colon
+
+    local new_entry="set_shell_autostart"
+    case ";$PROMPT_COMMAND;" in
+      *";$new_entry;"*)
+        # echo "PROMPT_COMMAND already contains: $new_entry"
+        :;;
+      *)
+        PROMPT_COMMAND="$PROMPT_COMMAND;$new_entry"
+        # echo "PROMPT_COMMAND does not contain: $new_entry"
+        ;;
+    esac
+  fi
+}
+
+shell_autostart_install
